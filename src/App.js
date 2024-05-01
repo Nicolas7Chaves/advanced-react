e370a8eff3bbfedcedfd70a1ebde14e8a7565d07
Nicolas7Chaves <nicolas7chaves@gmail.com>
2024-05-01 16:30:53 -0400

    button reusability

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import Button from './Reusability/Button';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button size='lg' variant="success">Button #1</Button>
+      <Button size='sm' variant='warning'>Button 2</Button>
     </div>
   );
 }
