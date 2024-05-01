81eebfc09c0c3f065de5c7d02d151d7a7c64577a
Nicolas7Chaves <nicolas7chaves@gmail.com>
2024-05-01 16:33:32 -0400

    moved files for simplcity

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import Button from './Reusability/Button';
-
+import ButtonApp from './Reusability/ButtonApp';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Button size='lg' variant="success">Button #1</Button>
-      <Button size='sm' variant='warning'>Button 2</Button>
-    </div>
+    <>
+      <ButtonApp />
+    </>
   );
 }
 
---
+++ src/Reusability/ButtonApp.js
@@ -0,0 +1,12 @@
+import Button from './Button';
+
+function ButtonApp() {
+  return (
+    <div className="App">
+      <Button size='lg' variant="success">Button #1</Button>
+      <Button size='sm' variant='warning'>Button 2</Button>
+    </div>
+  );
+}
+
+export default ButtonApp;
