5ee74b804086798af95dcb321c5de361f25c9ec0
Nicolas7Chaves <nicolas7chaves@gmail.com>
2024-05-10 12:04:40 -0400

    fix APp Parent component for Menu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Menu from './Reusability/Menu/Menu';
+import MenuButton from './Reusability/Menu/MenuButton';
+import MenuDropdown from './Reusability/Menu/MenuDropdown';
 
 function App() {
   return (
-    <>
-      <Menu
-        buttonText="Sports"
-        items={["Tennis", "Racquetball", "Pickleball", "Squash"]}
-      />
-    </>
+    <Menu>
+      <MenuButton>Sports</MenuButton>
+      <MenuDropdown items={["Tennis", "Pickleball", "Racquetball", "Squash"]} />
+    </Menu>
   );
 }
 
