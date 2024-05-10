157dbeb5bf426f8b3826625c9801a155eaa6ae60
Nicolas7Chaves <nicolas7chaves@gmail.com>
2024-05-10 12:02:34 -0400

    simplify menu button component to pass down childrena instead of buttonText

--- src/Reusability/Menu/MenuButton.js
+++ src/Reusability/Menu/MenuButton.js
@@ -1,11 +1,11 @@
 import '../styles.scss'
 import Button from '../Button/Button'
 
-export default function MenuButton({buttonText, onClick}) {
+export default function MenuButton({ onClick, children }) {
 
     return (
         <>
-            <Button onClick={onClick}>{buttonText}</Button>
+            <Button onClick={onClick}>{children}</Button>
         </>
     )
 }
